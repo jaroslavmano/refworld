cc0e7f98c007e8f927b7ae6ffea69988cf827241
Orim0cz <login@orim0.eu>
2021-06-24 14:45:16 +0200

    Hrubý náčrt Webu

--- index.php
+++ index.php
@@ -2,9 +2,32 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Dokument bez názvu</title>
+<title>RefWorld - Hlavní stránka</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Whitelist minecrft server">
+<meta name="keywords" content="Minecrft,  Whitelist, BzukCraft, Server, Minecrft server, mc server, origo, originální účet, minecraft 1.17">
+<meta name="author" content="Orim0">
 </head>
 
 <body>
+	<section>
+		<div>
+			<h1>RefWorld</h1>
+			<p>My jsme minecraft server</p>
+
+			<ul>
+				<li><span></span>Whitelist</li>
+				<li><span></span>Discord</li>
+				<li><span></span>Dynmap</li>
+			</ul>
+		</div>
+	</section>
+	
+	
+	<footer>
+	
+		<p>Website created by <a href="https://discord.gg/zVNtw6xBA2">Orim0</a></p>
+	
+	</footer>
 </body>
 </html>
